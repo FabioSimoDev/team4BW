8e64f32bb044f43ed3becdd872205bd9a98470d0
Nemanja Nisavic <diginautilus@gmail.com>
2023-09-20 11:56:16 +0200

    iniziato js

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,4 +1,27 @@
 // codice JavaScript della pagina
-const checkBoxLimiter = function () {
+const checkboxes = document.querySelectorAll(
+  "#difficult-checkboxs input[type=checkbox]"
+);
+
+const checkBoxLimiter = function (e) {
   // la farà nemanja
+  //const selectedCheckbox = e;
+  //checkboxes.forEach((element) => {
+  //  if (element.checked && element !== selectedCheckbox) {
+  //    console.log("ciao");
+  //  }
+  //});
+  let selectedCheckbox;
+  checkboxes.forEach((element) => {
+    if (element.checked) {
+      selectedCheckbox = element;
+    }
+    if (selectedCheckbox !== undefined) {
+      selectedCheckbox.checked = false;
+      if (selectedCheckbox !== e) {
+        selectedCheckbox.checked = false;
+        e.checked = true;
+      }
+    }
+  });
 };
