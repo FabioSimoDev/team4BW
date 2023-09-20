ff46ed8589c4ecca6feae12c8f510d31cca4618b
FabioSimoDev <fabiofabietto182@gmail.com>
2023-09-20 16:05:54 +0200

    aggiunti i query parameters

--- welcome page/script.js
+++ welcome page/script.js
@@ -5,6 +5,8 @@ const checkboxes = document.querySelectorAll(
   "#difficult-checkboxs input[type=checkbox]"
 );
 
+let selectedCheckBoxDefinitive;
+
 const checkBoxLimiter = function (e) {
   let selectedCheckbox; //variabile che conterrà la checkbox selezionata
   checkboxes.forEach((element) => {
@@ -20,6 +22,19 @@ const checkBoxLimiter = function (e) {
     if (selectedCheckbox !== e.children[0]) {
       selectedCheckbox.checked = false; //disabilita la checkbox che era precedentemente selezionata
       e.children.checked = true; //abilita la checkbox cliccata ora
+
+      console.log(selectedCheckBoxDefinitive);
     }
   }
+  selectedCheckBoxDefinitive = e; //salva la checkbox cliccata come quella definitiva
+};
+
+const changePage = function () {
+  const difficulty = selectedCheckBoxDefinitive.textContent.trim();
+  console.log(difficulty);
+  const url = `../benchmark page/index.html?difficulty=${difficulty}`;
+  window.location.href = url;
+  //prima pagina.html --> prima pagina.html?difficulty+easy
+  //seconda pagina.html --> const difficulty = difficulty (easy)
+  //http://127.0.0.1:5500/welcome%20page/index.html --> http://127.0.0.1:5500/welcome%20page/index.html?difficulty=easy
 };
