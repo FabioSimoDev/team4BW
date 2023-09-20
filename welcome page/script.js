33cf1aef72ebdf83ce054c193e3e21496bedc37d
FabioSimoDev <fabiofabietto182@gmail.com>
2023-09-20 14:52:55 +0200

    css e js

--- welcome page/script.js
+++ welcome page/script.js
@@ -1,27 +1,25 @@
 // codice JavaScript della pagina
+
+// usiamo querySelectorAll per reperire tutte le checkbox all'interno del contenitore 'difficult-checkboxs'
 const checkboxes = document.querySelectorAll(
   "#difficult-checkboxs input[type=checkbox]"
 );
 
 const checkBoxLimiter = function (e) {
-  // la farà nemanja
-  //const selectedCheckbox = e;
-  //checkboxes.forEach((element) => {
-  //  if (element.checked && element !== selectedCheckbox) {
-  //    console.log("ciao");
-  //  }
-  //});
-  let selectedCheckbox;
+  let selectedCheckbox; //variabile che conterrà la checkbox selezionata
   checkboxes.forEach((element) => {
+    //ciclo tutte le checkbox
     if (element.checked) {
       selectedCheckbox = element;
     }
-    if (selectedCheckbox !== undefined) {
-      selectedCheckbox.checked = false;
-      if (selectedCheckbox !== e) {
-        selectedCheckbox.checked = false;
-        e.checked = true;
-      }
-    }
   });
+
+  //se una checkbox selezionata è stata trovata
+  if (selectedCheckbox !== undefined) {
+    //se la checkbox selezionata non è la stessa di quella cliccata
+    if (selectedCheckbox !== e.children[0]) {
+      selectedCheckbox.checked = false; //disabilita la checkbox che era precedentemente selezionata
+      e.children.checked = true; //abilita la checkbox cliccata ora
+    }
+  }
 };
